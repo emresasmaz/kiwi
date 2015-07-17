2d21d990932f519e32c341b89d492ecbad556a1b
emre <sasmazemre7@gmail.com>
2015-07-16 19:36:02 -0700

    Fix delete

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/confessions/new' => 'confessions#new'
   post '/confessions' => 'confessions#create'
   get '/confessions/:id', to: 'confessions#show', as: 'confession'
-  delete '/confession/:id' , to: 'confessions#destroy' , as: 'delete_confession'
+  delete '/confessions/:id' , to: 'confessions#destroy' , as: 'delete_confession'
   
 
   # Example of regular route:
