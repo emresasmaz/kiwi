7229f775a1fd6606a27b0825fdd68820b3d2759c
emre <sasmazemre7@gmail.com>
2015-07-22 18:09:00 -0700

    Scroll bar added to index page.

--- app/models/confession.rb
+++ app/models/confession.rb
@@ -1,5 +1,5 @@
 class Confession < ActiveRecord::Base 
   belongs_to :user
-  validates :content, presence: true, length: { maximum: 300 } 
+  validates :content, presence: true, length: { maximum: 600 } 
   validates :title, presence: true, length: { minimum: 4, maximum: 50 }
 end
